1e2a1a1c8ae19dd5a14ac1a2a0e0e9411d427347
Gahjouyooj <tyiwei0@gmail.com>
2023-01-19 20:19:12 +0800

    Completed efficiency test

--- TEST/Efficiency testing/Efficiency.cpp
+++ TEST/Efficiency testing/Efficiency.cpp
@@ -5,29 +5,42 @@
 
 typedef std::chrono::high_resolution_clock Clock;
 
-// #include <iostream>
-// #include <chrono>
-// typedef std::chrono::high_resolution_clock Clock;
-// main() {
-//    auto start_time = Clock::now();
-//    for(int i = 0; i<200000000; i++ ){ //create one empty loop
-//       ;
-//    }
-//    auto end_time = Clock::now();
-//    std::cout << "Time difference: " << std::chrono::duration_cast<std::chrono::nanoseconds>(end_time    - start_time).count() << " nanoseconds" << std::endl;
-// }
-
 int main()
 {
+    int count = 100000000;
     auto start_time = Clock::now();
-    for (int i = 0; i < 1000000000; i++)
+    for (int i = 0; i < count; i++)
     {
         double a;
-        a = 1000 * 0.2;
+        a = 1000 / 5.0;
     }
     auto end_time = Clock::now();
     auto deltatime = end_time - start_time;
 
-    std::cout << std::chrono::duration_cast<std::chrono::nanoseconds>(deltatime).count() << std::endl;
+    std::cout << "floating point division: " << std::chrono::duration_cast<std::chrono::nanoseconds>(deltatime).count() << std::endl;
+
+    start_time = Clock::now();
+    for (int i = 0; i < count; i++)
+    {
+        double a;
+        a = 1000 / 5;
+    }
+    end_time = Clock::now();
+    deltatime = end_time - start_time;
+
+    std::cout << "division: " << std::chrono::duration_cast<std::chrono::nanoseconds>(deltatime).count() << std::endl;
+
+    start_time = Clock::now();
+    for (int i = 0; i < count; i++)
+    {
+        double a;
+        a = 1000 * 0.2;
+    }
+    end_time = Clock::now();
+    deltatime = end_time - start_time;
+    std::cout << "floating point multiplication: " << std::chrono::duration_cast<std::chrono::nanoseconds>(deltatime).count() << std::endl;
+
     return 0;
 }
+
+/*Conclusion: floating point division > division > floating point multiplication*/
